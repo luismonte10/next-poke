5a33c3d7d097f41b955a689806a90b343f150211
Luís Monte <luismonte.l560@gmail.com>
2023-04-13 19:56:27 -0300

    feat: change logo and include name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,28 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <ul className={styles.navbar}>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/about">Sobre</Link>
-      </li>
-      <li>
-        <Link href="/todos">Tarefas</Link>
-      </li>
-    </ul>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image
+          src="/images/pokeball.png"
+          width="30"
+          height="30"
+          alt="PokeNext logo"
+        />
+        <h1>PokeNext</h1>
+      </div>
+      <ul className={styles.link_items}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">Sobre</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
