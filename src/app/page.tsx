2909e63c66c2662fbaeb6f3547c3f0126385569c
Luís Monte <luismonte.l560@gmail.com>
2023-04-30 15:46:56 -0300

    feat: Home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,48 @@
-export default function Home() {
-  return <h1>Hello World Next.js</h1>;
+import Image from 'next/image';
+
+import styles from '../styles/Home.module.css';
+
+type pokemonsProps = {
+  id: number;
+  name: string;
+  url: string;
+};
+
+export async function getPokemons() {
+  const maxPokemons = 251;
+  const url = 'https://pokeapi.co/api/v2/pokemon';
+
+  const data = await fetch(`${url}/?limit=${maxPokemons}`);
+  const pokemons = await data.json();
+
+  pokemons.results.forEach((item: pokemonsProps, index: number) => {
+    item.id = index + 1;
+  });
+
+  return pokemons.results;
+}
+
+export default async function Home() {
+  const pokemons: pokemonsProps[] = await getPokemons();
+
+  return (
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokeNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <p key={pokemon.id}>{pokemon.name}</p>
+        ))}
+      </div>
+    </>
+  );
 }
